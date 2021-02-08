9e42484b68c03b0014b7f2c3cf7b1c589165c0a5
Miguel <miguellukas52@gmail.com>
2021-02-08 20:01:14 -0300

    exerc 3 teorica

---
+++ teorica/exerc-3/build/CMakeFiles/exerc-3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exerc-3.dir/Pedagio.cpp.o"
+  "CMakeFiles/exerc-3.dir/main.cpp.o"
+  "exerc-3"
+  "exerc-3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exerc-3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
