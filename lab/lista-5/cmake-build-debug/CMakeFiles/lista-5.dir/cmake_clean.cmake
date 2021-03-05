1bfc8ff7481f5242dca780b78d79c72b4dfb15d3
Miguel <miguellukas52@gmail.com>
2021-03-05 12:57:02 -0300

    lista 5 lab

---
+++ lab/lista-5/cmake-build-debug/CMakeFiles/lista-5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lista-5.dir/main.cpp.o"
+  "lista-5"
+  "lista-5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lista-5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab/lista-5/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by miguel on 05/03/2021.
+//
+
